4aa424a47d5c5ed5e345abb827e6536223d6eb15
wuze <zwubn@connect.ust.hk>
2022-04-27 12:31:43 +0800

    lab9

---
+++ src/lab9/FindMax.java
@@ -0,0 +1,89 @@
+package lab9;
+
+/**
+ *
+ * COMP 3021
+ *
+This is a class that prints the maximum value of a given array of 90 elements
+
+This is a single threaded version.
+
+Create a multi-thread version with 3 threads:
+
+one thread finds the max among the cells [0,29]
+another thread the max among the cells [30,59]
+another thread the max among the cells [60,89]
+
+Compare the results of the three threads and print at console the max value.
+
+ *
+ * @author valerio
+ *
+ */
+public class FindMax {
+	// this is an array of 90 elements
+	// the max value of this array is 9999
+	int newmax1=0;
+	int newmax2=0;
+	int newmax3=0;
+	static int[] array = { 1, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2, 3, 4543,
+			234, 3, 454, 1, 2, 3, 1, 9999, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3, 1, 34, 5, 6, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3 };
+
+	public static void main(String[] args) {
+		new FindMax().printMax();
+	}
+
+	public void printMax() {
+		// this is a single threaded version
+
+		Thread max1t=new Thread(()->{
+			newmax1=findMax(0,29);
+		});
+		Thread max2t=new Thread(()->{
+
+
+			newmax2=findMax(30,59);
+		});
+		Thread max3t=new Thread(()->{
+
+			newmax3=findMax(60,89);
+		});
+		max1t.start();
+		max2t.start();
+		max3t.start();
+		try{
+			max1t.join();
+			max2t.join();
+			max3t.join();
+
+		}catch(Exception e)
+		{
+
+		}
+		int newmax=Math.max(newmax1,newmax2);
+		newmax=Math.max(newmax, newmax3);
+
+
+		System.out.println("the max value is " + newmax);
+	}
+
+	/**
+	 * returns the max value in the array within a give range [begin,range]
+	 *
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	private int findMax(int begin, int end) {
+		// you should NOT change this function
+		int max = array[begin];
+		for (int i = begin + 1; i <= end; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+}
